8f5a413a07f0dc88f58060869b07c37f964d3b2f
MicrosoftAdmin <MicrosoftAdmin@DESKTOP-1RBS808>
2026-02-19 10:05:23 +0000

    initial setup

---
+++ MarsRover.Tests/HelloWorldTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+namespace MarsRover.Tests;
+
+public class HelloWorldTests
+    {
+    [Test]
+    public void Successfully_runs_a_test()
+        {
+        Assert.That(true, Is.True);
+        }
+    }
---
+++ MarsRoverProject/Directions.cs
@@ -0,0 +1,21 @@
+﻿namespace Direction
+    {
+    internal class Directions
+        {
+        enum InstructionType
+            {
+            L,
+            R,
+            M
+            }
+        enum DirectionType
+            { 
+            N,
+            E,
+            S,
+            W
+            }
+        }
+
+        
+    }
